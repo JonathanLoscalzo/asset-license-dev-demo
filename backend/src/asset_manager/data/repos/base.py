8149d794ebbe0debc8d06678272eeae5eef9930f
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 18:23:54 -0300

    add update item

--- backend/src/asset_manager/data/repos/base.py
+++ backend/src/asset_manager/data/repos/base.py
@@ -1,4 +1,6 @@
 from typing import Any, TypeVar, Generic
+from bson.objectid import ObjectId
+from pymongo.collection import ReturnDocument
 from pymongo.database import Database
 from asset_manager.data.exceptions import ItemNotFound
 
@@ -21,14 +23,31 @@ class MongoRepository(Generic[T]):
             map(self.generic_class.parse_obj, self.collection().find({}))
         )
 
-    def get(self, id: str):
+    def get(self, id: str) -> T:
 
-        item = self.collection().find_one({"$or": [{"id": id}, {"_id": id}]})
+        oid = ObjectId(id) if ObjectId.is_valid(id) else id
+        item = self.collection().find_one({"$or": [{"id": id}, {"_id": oid}]})
 
         if item is None:
             raise ItemNotFound(id)
 
         return self.generic_class(**item)
 
+    def get_by_filter(self, filter: dict) -> T:
+        return self.collection().find_one(filter)
+
     def add(self, obj: T) -> Any:
         return self.collection().insert_one(obj.dict()).inserted_id
+
+    def update(self, id, obj) -> Any:
+        oid = ObjectId(id) if ObjectId.is_valid(id) else id
+        res = self.collection().find_one_and_update(
+            {"$or": [{"id": id}, {"_id": oid}]},
+            obj,
+            return_document=ReturnDocument.AFTER,
+        )
+
+        return res
+
+    def remove(self, id) -> Any:
+        raise NotImplementedError()
