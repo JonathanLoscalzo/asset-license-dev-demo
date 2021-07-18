8ae1556201b6ca34818e82103a256aa86ff6140b
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 18:40:29 -0300

    edit devservice dependencies

--- backend/src/asset_manager/deps/services.py
+++ backend/src/asset_manager/deps/services.py
@@ -1,11 +1,19 @@
 from fastapi import Depends
+from asset_manager.data.repos.assets import AssetRepository
 
 from asset_manager.data.repos.developers import DeveloperRepository
+from asset_manager.data.repos.license import LicenseRepository
 from asset_manager.data.services.developer import DeveloperService
-from asset_manager.deps import get_dev_repository
+from asset_manager.deps.repositories import (
+    get_asset_repository,
+    get_license_repository,
+    get_dev_repository
+)
 
 
 def get_developer_service(
     repository: DeveloperRepository = Depends(get_dev_repository),
+    asset_repo: AssetRepository = Depends(get_asset_repository),
+    license_repo: LicenseRepository = Depends(get_license_repository),
 ):
-    return DeveloperService(repository)
+    return DeveloperService(repository, asset_repo, license_repo)
