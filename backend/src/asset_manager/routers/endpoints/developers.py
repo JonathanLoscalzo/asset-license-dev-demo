e339fecb0a9637300bbf7d2f34e17bdccfb69a86
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 12:20:48 -0300

    add create developer endpoint

--- backend/src/asset_manager/routers/endpoints/developers.py
+++ backend/src/asset_manager/routers/endpoints/developers.py
@@ -1,6 +1,9 @@
 from typing import List
+from asset_manager.data.repos.developers import DeveloperRepository
+from asset_manager.data.services.developer import DeveloperService
+from asset_manager.models.auth import LoginUser
 from fastapi import APIRouter, Depends
-from asset_manager.deps import get_current_user
+from asset_manager.deps import get_asset_repository, get_current_user, get_developer_service
 
 from asset_manager.models.models import (
     Asset,
@@ -18,13 +21,22 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[FullDeveloper])
-async def get_developers():
-    pass
+async def get_developers(
+    current_user: LoginUser = Depends(get_current_user),
+    repo: DeveloperRepository = Depends(get_asset_repository),
+):
+    devs = repo.get_all()
+
+    return devs
 
 
 @router.post("/", response_model=Developer)
-async def add_developer(developer: CreateDev):
-    pass
+async def add_developer(
+    developer: CreateDev,
+    service: DeveloperService = Depends(get_developer_service),
+):
+    return service.add(developer)
+    
 
 
 @router.post("/{id}/activate")
