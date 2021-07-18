168d80aad177d8d9ddd938234307b63a0c2a2b0d
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 12:19:55 -0300

    add DeveloperRepository

---
+++ backend/src/asset_manager/data/repos/developers.py
@@ -0,0 +1,10 @@
+from fastapi.params import Depends
+from asset_manager.data.schemas.developer import DeveloperMongo
+from pymongo.database import Database
+from asset_manager.data.repos.base import MongoRepository
+
+
+class DeveloperRepository(MongoRepository[DeveloperMongo]):
+    def __init__(self, db: Database = Depends()):
+        super().__init__(db, "developers", DeveloperMongo)
+
