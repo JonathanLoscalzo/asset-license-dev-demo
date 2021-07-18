3bcb55791b68437a749edbbfb127b4fae06f359f
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 12:29:21 -0300

    remove dependency due to cycle

--- backend/src/asset_manager/data/repos/developers.py
+++ backend/src/asset_manager/data/repos/developers.py
@@ -1,10 +1,8 @@
-from fastapi.params import Depends
 from asset_manager.data.schemas.developer import DeveloperMongo
 from pymongo.database import Database
 from asset_manager.data.repos.base import MongoRepository
 
 
 class DeveloperRepository(MongoRepository[DeveloperMongo]):
-    def __init__(self, db: Database = Depends()):
+    def __init__(self, db: Database):
         super().__init__(db, "developers", DeveloperMongo)
-
