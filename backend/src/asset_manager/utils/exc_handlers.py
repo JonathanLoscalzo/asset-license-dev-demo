06ab736190cfd7621da7f41cfcf598a27597970d
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 18:23:40 -0300

    add exc_handler install dynamically

--- backend/src/asset_manager/utils/exc_handlers.py
+++ backend/src/asset_manager/utils/exc_handlers.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from asset_manager.data.exceptions.base import ItemNotFound
+import importlib
+from fastapi.exceptions import HTTPException
+
+# from fastapi.responses import JSONResponse
+from asset_manager.data.exceptions.base import (
+    AssetManagerException,
+)
+from fastapi.exception_handlers import http_exception_handler
+
 
 def add_exceptions_handlers(app: FastAPI):
-    @app.exception_handler(ItemNotFound)
-    def itemnotfound_handler(request: Request, exc: ItemNotFound):
-        return JSONResponse(status_code=404, content={"message": exc.message})
+
+    exceptions = importlib.import_module("asset_manager.data.exceptions")
+
+    exceptions = [
+        (name, cls)
+        for name, cls in exceptions.__dict__.items()
+        if type(cls) is type and AssetManagerException in cls.mro()
+    ]
+
+    async def generic_handler(request: Request, exc):
+        ex = HTTPException(status_code=404, detail={"message": exc.message})
+        return await http_exception_handler(request, ex)
+
+    for _name, cls in exceptions:
+        app.exception_handler(cls)(generic_handler)
+
+    # TODO: log handlers
