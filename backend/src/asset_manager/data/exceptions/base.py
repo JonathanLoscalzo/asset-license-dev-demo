5dfd5ee40c689dedc317d00a25ffea4499e9d1b3
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2021-07-18 18:22:44 -0300

    add new exceptions

--- backend/src/asset_manager/data/exceptions/base.py
+++ backend/src/asset_manager/data/exceptions/base.py
@@ -1,13 +1,41 @@
-
-
-class ItemNotFound(Exception):
+class AssetManagerException(Exception):
     message: str
+
+    def __init__(self):
+        super().__init__(self.message)
+
+
+class ItemNotFound(AssetManagerException):
     def __init__(self, id):
         self.id = id
         self.message = f"Item {id} not found"
-        super().__init__(self.message)
+        super().__init__()
 
-class MethodShouldNotBeImplemented(Exception):
-    def __init__(self, method:str):
+
+class MethodShouldNotBeImplemented(AssetManagerException):
+    def __init__(self, method: str):
         self.method = method
-        super().__init__(f"Method {method} should not be implemented")
+        self.message = f"Method {method} should not be implemented"
+        super().__init__()
+
+
+class AssetHasAssignedException(AssetManagerException):
+    def __init__(self, asset_id):
+        self.message = f"Asset {asset_id} just have a user!"
+        super().__init__()
+
+
+class LicenseHasCurrentUserException(AssetManagerException):
+    def __init__(self, license_id, dev_id):
+        self.message = (
+            f"License({license_id}) has just been assigned to User({dev_id})"
+        )
+        super().__init__()
+
+
+class UserJustHaveAssetException(AssetManagerException):
+    def __init__(self, dev_id, asset_id):
+        self.message = (
+            f"Developer {dev_id} just have a this asset ({asset_id})!"
+        )
+        super().__init__()
